213018c4ad42a53430005885e2965e4da9347c67
vunam <vuvannamb1@gmail.com>
2025-09-11 19:26:05 +0700

    update db conect3

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,36 +3,36 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// export const pool = mysql.createConnection(process.env.MYSQL_URL);
-    // host: process.env.DB_HOST,
-    // port: Number(process.env.DB_PORT||3306),
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
+export const pool = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT||3306),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 
-    // waitForConnections:true,
-    // connectionLimit:10,
-    // queueLimit:0,
-    // timezone:'Z',
-    // supportBigNumbers:true,
-    // dateStrings:true,
-    // namedPlaceholders:true,});
-
-export const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+    waitForConnections:true,
+    connectionLimit:10,
+    queueLimit:0,
     timezone:'Z',
     supportBigNumbers:true,
     dateStrings:true,
     namedPlaceholders:true,
 });
+
+// export const pool = mysql.createPool({
+//     host: process.env.MYSQLHOST,
+//     port: process.env.MYSQLPORT,
+//     user: process.env.MYSQLUSER,
+//     password: process.env.MYSQLPASSWORD,
+//     database: process.env.MYSQLDATABASE,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+//     timezone:'Z',
+//     supportBigNumbers:true,
+//     dateStrings:true,
+//     namedPlaceholders:true,
+// });
 (async () => {
     try {
         const connection = await pool.getConnection();
