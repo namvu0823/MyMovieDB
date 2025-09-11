f673aec1811862d7afacb787cb1d73a31c2683b7
vunam <vuvannamb1@gmail.com>
2025-09-11 19:41:56 +0700

    update db conect5

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT||3306),
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -19,20 +19,7 @@ export const pool = mysql.createPool({
     namedPlaceholders:true,
 });
 
-// export const pool = mysql.createPool({
-//     host: process.env.MYSQLHOST,
-//     port: process.env.MYSQLPORT,
-//     user: process.env.MYSQLUSER,
-//     password: process.env.MYSQLPASSWORD,
-//     database: process.env.MYSQLDATABASE,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0,
-//     timezone:'Z',
-//     supportBigNumbers:true,
-//     dateStrings:true,
-//     namedPlaceholders:true,
-// });
+
 (async () => {
     try {
         const connection = await pool.getConnection();
