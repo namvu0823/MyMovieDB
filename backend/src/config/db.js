2ec1ffe10d09aac5504a824f90b595259fd85fdf
vunam <vuvannamb1@gmail.com>
2025-09-11 19:07:52 +0700

    update db conect

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,21 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT||3306),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+export const pool = mysql.createPool(process.env.MYSQL_URL);
+    // host: process.env.DB_HOST,
+    // port: Number(process.env.DB_PORT||3306),
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
 
-    waitForConnections:true,
-    connectionLimit:10,
-    queueLimit:0,
-    timezone:'Z',
-    supportBigNumbers:true,
-    dateStrings:true,
-    namedPlaceholders:true,
-});
+    // waitForConnections:true,
+    // connectionLimit:10,
+    // queueLimit:0,
+    // timezone:'Z',
+    // supportBigNumbers:true,
+    // dateStrings:true,
+    // namedPlaceholders:true,});
 
 
 (async () => {
