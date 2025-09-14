aa542fc1bba9a0b0da705a36608a544dbdd1d401
vunam <vuvannamb1@gmail.com>
2025-09-15 00:14:18 +0700

    update frontend_url

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API_BASE = process.env.REACT_APP_API_URL;
 
 const api=axios.create({
-    baseURL:"https://mymoviedb-production.up.railway.app",
+    baseURL:API_BASE,
     timeout:5000,
     headers:{
         "Content-Type":"application/json"
