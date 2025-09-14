fb4deb21ddbd5d1b06525fe90384fe5b242728f8
vunam <vuvannamb1@gmail.com>
2025-09-14 20:15:39 +0700

    change debug4

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,6 +3,11 @@
 //   dotenv.config({ path: "./.env" });
 // }
 
+console.log("ENV DEBUG:", {
+  keys: Object.keys(process.env),
+  TMDB_AUTH: process.env.TMDB_AUTH ? "EXISTS" : "MISSING",
+});
+
 export const PORT = process.env.PORT;
 export const TMDB_AUTH=process.env.TMDB_AUTH;
 export const JWT_SECRET= process.env.JWT_SECRET;
